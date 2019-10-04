a30571cf1c693a770d5d1fb124cbd3be823e24d0
kozr <zxzflow@gmail.com>
2019-10-04 12:27:48 -0700

    updating

--- doNotRemove.sh
+++ doNotRemove.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git pull
 git status > changes.txt
-cmp --silent doNotRemove.txt changes.txt && echo '### No updates ###' || (echo '### Updating ###' && git add -A && git commit -m "updating" && git push)
-rm changes.txt
+cmp --silent doNotRemove.txt changes.txt && echo '### No updates ###' || (echo '### Updating ###' && rm changes.txt && git add -A && git commit -m "updating" && git push)
