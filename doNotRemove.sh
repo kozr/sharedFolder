3ed4bb9a8ba564084d0194df6b56369c87062fc7
kozr <zxzflow@gmail.com>
2019-12-28 19:34:05 -0800

    commit

--- doNotRemove.sh
+++ doNotRemove.sh
@@ -8,5 +8,5 @@ do
 git pull
 git status > git-changes.txt
 cmp -s git-status.txt git-changes.txt && echo '### No updates ###' || ( echo '### Updating ###' && git add -A && git commit -m "updating" && git push && rm git-changes.txt )
-sleep 60
+sleep 10
 done
