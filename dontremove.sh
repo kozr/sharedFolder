dde1d30c4570501f3d9b079e5c3619f7ca46f3b4
fakeoliviali <fakeoliviali@gmail.com>
2019-12-30 14:27:48 -0800

    commit

--- dontremove.sh
+++ dontremove.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m "commit"
 git push
 numberLines= wc -l contributor.txt
-while [ $numberLines != $contributorNumber ]
+while [ "$numberLines" != "$contributorNumber" ]
 do
 git pull
 sleep 10
